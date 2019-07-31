17fae2c9bbea046c28a75b54900f3f1ddd30615f
ThraxHamilton <mmcclenton16@gmail.com>
2019-07-31 12:32:00 -0500

    part 4 & 5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
 import todoData from './components/todoData'
 
-function App() {
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      todos:[]
+    }
+  }
+  render(){
   // todoItems is a new array being created from MAPPING the array in todoData. Make a taco, and use it to access the info you get back from MAPPING over todoDate. 
+  const todoItems = todoData.map(taco => <TodoItem key={taco.id} taco={taco} />)
   // taco = todoData.map
   // Feed the taco to TodoItem
-  const todoItems = todoData.map(taco => <TodoItem key={taco.id} taco={taco} />)
+    return (
+      <div className='to-do-list'>
+          {todoItems}
+      </div>
+    );
   
-  return (
-    <div className='to-do-list'>
-        {todoItems}
-    </div>
-  );
+}
 }
 
-export default App;
+;
